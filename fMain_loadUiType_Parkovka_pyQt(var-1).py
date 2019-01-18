f496eccd954c446dc843ad7cbe8535a0c52d4f86
Dmitry <dmitry.makarkin@gmail.com>
2019-01-18 15:53:11 +0600

    Поправлен путь местонахождения файла

--- fMain_loadUiType_Parkovka_pyQt(var-1).py
+++ fMain_loadUiType_Parkovka_pyQt(var-1).py
@@ -5,7 +5,7 @@ from PyQt5 import QtWidgets, uic
 class MyWindow(QtWidgets.QWidget):
     def __init__(self, parent = None):
         QtWidgets.QWidget.__init__(self, parent)
-        Form, Base = uic.loadUiType("D:\MyProjects\MyWorkInPython\Parkovka_pyQt\Parkovka_pyQt.git\Parkovka_pyQt(form).ui")
+        Form, Base = uic.loadUiType('D:/MyProjects/MyWorkInPython/Parkovka_pyQt/Parkovka_pyQt.git/Parkovka_pyQt(form).ui')
         self.ui = Form()
         self.ui.setupUi(self)
 
